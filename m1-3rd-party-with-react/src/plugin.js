00920e9fd44de4f8accabaf61ea5c9fbbfed1f24
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-02-07 23:25:01 -0300

    module 1 done

---
+++ m1-3rd-party-with-react/src/plugin.js
@@ -0,0 +1,24 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class Plugin extends Component {
+  constructor () {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick () {
+    console.log('clicou no btn', this.myInput)
+    this.myInput.focus()
+  }
+  render () {
+    return (
+      <div>
+        <input type='text' ref={(node) => (this.myInput = node)} />
+        <button onClick={this.handleClick}>Focus</button>
+      </div>
+    )
+  }
+}
+
+export default Plugin
