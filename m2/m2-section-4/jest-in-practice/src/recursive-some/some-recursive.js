4e8e6770203b3a33a21602df69eb8ec5dc06a4d6
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-02-16 21:12:13 -0300

    TDD - Imperative and Recursive functions

---
+++ m2/m2-section-4/jest-in-practice/src/recursive-some/some-recursive.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// Imperative function:
+/*
+const some = (arr, func) => {
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (func(arr[i], i, arr))
+      return true
+  }
+  return false
+}
+*/
+
+
+// Recursive function:
+
+
+const some = (arr = [], func = (item) => item) => {
+
+  return (function everyInternal(arrayInternal, counter) {
+    const [head, ...tail] = arrayInternal
+
+    return arrayInternal.length === 0
+      ? false
+      : func(head, counter, arr)
+        ? true
+        : everyInternal(tail, counter + 1)
+  })(arr, 0)
+}
+
+//console.log(every([], (item) => item))
+export default some
