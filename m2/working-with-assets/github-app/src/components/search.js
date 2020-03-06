c102c0ea9b066084e41bea824c88249980a42849
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-03-06 20:07:28 -0300

    css loaders

---
+++ m2/working-with-assets/github-app/src/components/search.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import React, { PropTypes } from 'react'
+
+const Search = ({ isDisabled, handleSearch }) => (
+  <div className='search'>
+    <input
+      type='search'
+      placeholder={'Enter GitHub user\'s name'}
+      disabled={isDisabled}
+      onKeyUp={handleSearch}
+    />
+  </div>
+)
+
+Search.propTypes = {
+  handleSearch: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool.isRequired
+}
+
+export default Search
