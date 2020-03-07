25955c71fa8af102da6fd66f9cecf8228e533226
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-03-07 14:15:33 -0300

    bundle generate

--- m2/working-with-assets/github-app/webpack.prod.config.js
+++ m2/working-with-assets/github-app/webpack.prod.config.js
@@ -8,24 +8,29 @@ const htmlPlugin = require('html-webpack-plugin')
 const extractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    devtool: 'sourcemap',
 
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        path.join(__dirname, 'src', 'index')
-    ],
+    entry: path.join(__dirname, 'src', 'index'),
 
     output: {
         path:path.join(__dirname, 'dist'),
-        filename: '[name]-[hash].js',
-        publicPath: ''
+        filename: '[name]-[hash].js'
     },
 
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
         new extractTextPlugin('[name]-[hash].css'),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        }),
+
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+
         new htmlPlugin({
             title: 'Github app',
             template: path.join(__dirname, 'src', 'html', 'template.html')
@@ -49,7 +54,7 @@ module.exports = {
             test: /\.css$/,
             exclude: /node_modules/,
             include: /src/,
-            loaders: ['style', 'css']
+            loader: extractTextPlugin.extract('style', 'css')
         }]
     }
 }
