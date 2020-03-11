a5d2ced912467f4603d75ba87352cf417f27938e
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-03-11 16:29:51 -0300

    linter sucks

--- m2/working-with-assets/github-app/webpack.prod.config.js
+++ m2/working-with-assets/github-app/webpack.prod.config.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 const webpack = require('webpack')
-const validate = require('webpack-validator')
+// const validate = require('webpack-validator')
 
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
@@ -12,61 +12,62 @@ const styles = new ExtractTextPlugin('[name]-[hash].css')
 
 module.exports = {
 
-    entry: path.join(__dirname, 'src', 'index'),
+  entry: path.join(__dirname, 'src', 'index'),
 
-    output: {
-        path:path.join(__dirname, 'dist'),
-        filename: '[name]-[hash].js'
-    },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]-[hash].js'
+  },
 
-    plugins: [
-        crp,
-        styles,
+  plugins: [
+    crp,
+    styles,
 
-        new ExtractTextPlugin('[name]-[hash].css'),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': '"production"'
-            }
-        }),
+    new ExtractTextPlugin('[name]-[hash].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
 
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
 
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
 
-        new HtmlPlugin({
-            title: 'Github app',
-            inject: false,
-            template: path.join(__dirname, 'src', 'html', 'template.html')
-        })
-    ],
+    new HtmlPlugin({
+      title: 'Github app',
+      inject: false,
+      template: path.join(__dirname, 'src', 'html', 'template.html')
+    })
+  ],
 
-    module: {
-        preLoaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'standard'
-        }],
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'standard'
+    }],
 
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'babel'
-        }, {
-            test: /\.css$/,
-            exclude: /node_modules|(search|style)\.css/,
-            include: /src/,
-            loader: styles.extract('style', 'css?modules')
-        }, {
-            test: /(search|style)\.css$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: crp.extract('style', 'css?modules')
-        }]
-    }
+    loaders: [{
+      test: /\.js$/,
+      // exclude: /node_modules/,
+      // include: /src/,
+      include: path.join(__dirname, 'src'),
+      loader: 'babel'
+    }, {
+      test: /\.css$/,
+      exclude: /node_modules|(search|style)\.css/,
+      include: /src/,
+      loader: styles.extract('style', 'css?modules')
+    }, {
+      test: /(search|style)\.css$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: crp.extract('style', 'css?modules')
+    }]
+  }
 }
