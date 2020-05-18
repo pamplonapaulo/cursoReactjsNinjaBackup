f8f84e100adc76d01b22c8365c8abd53eda2eae8
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-05-18 09:30:22 -0300

    pagination done

--- m2/github-app/src/components/repos/index.js
+++ m2/github-app/src/components/repos/index.js
@@ -8,27 +8,35 @@ const Repos = ({ type, title, repos, handlePagination }) => (
   <div className={style[type]}>
     <h2>{title}</h2>
     <ul>
-      {repos.map((repo, index) => (
+      {repos.repos.map((repo, index) => (
         <li key={index}>
           <a href={repo.link}>{repo.name}</a>
         </li>
       ))}
     </ul>
 
-    <Pagination total={10} activePage={3} onClick={handlePagination} />
+    <Pagination total={repos.pagination.total} activePage={repos.pagination.activePage} onClick={handlePagination} />
   </div>
 )
 
 Repos.defaultProps = {
-  type: '',
-  repos: []
+  type: ''
 }
 
 Repos.propTypes = {
   type: PropTypes.string,
   title: PropTypes.string.isRequired,
   handlePagination: PropTypes.func.isRequired,
-  repos: PropTypes.array
+  repos: PropTypes.shape({
+    repos: PropTypes.arrayOf(PropTypes.shape({
+      link: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })).isRequired,
+    pagination: PropTypes.shape({
+      total: PropTypes.number,
+      activePage: PropTypes.number
+    }).isRequired
+  })
 }
 
 export default Repos
