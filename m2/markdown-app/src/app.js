93693855cecdac6125f4115e2e136b2d07e8ed29
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-06-10 15:10:41 -0300

    uglify error on build - class 100 M2

--- m2/markdown-app/src/app.js
+++ m2/markdown-app/src/app.js
@@ -1,30 +1,32 @@
 'use strict'
 
 import React, { Component } from 'react'
+import marked from 'marked'
+import highlight from 'highlight.js'
 import MardownEditor from './markdown-editor'
 
 import './css/style.css'
 
+marked.setOptions({
+  highlight: (code) => {
+    return highlight.highlightAuto(code).value
+  }
+})
 
 class App extends Component {
   constructor () {
     super()
-    this.state = { value: ''}
+    this.state = { value: '' }
 
     this.handleChange = (e) => {
-
       this.setState({
         value: e.target.value
       })
     }
-  }
 
-  handleSubmit (e) {
-    e.preventDefault()
-    
-    this.setState({
-      value: e.target.textarea.value
-    })
+    this.getMarkup = () => {
+      return { __html: marked(this.state.value) }
+    }
   }
 
   render () {
@@ -32,6 +34,7 @@ class App extends Component {
       <MardownEditor
         value={this.state.value}
         handleChange={this.handleChange}
+        getMarkup={this.getMarkup}
       />
     )
   }
