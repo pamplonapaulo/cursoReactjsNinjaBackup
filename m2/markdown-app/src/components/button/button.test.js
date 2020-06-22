334efdc2303f75660ea038d4d20f3a25cb4fc625
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-06-22 15:21:40 -0300

    module 2 done

--- m2/markdown-app/src/components/button/button.test.js
+++ m2/markdown-app/src/components/button/button.test.js
@@ -1,12 +1,21 @@
 'use strict'
 
+// setup file (yup, lazy I am.)
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+configure({ adapter: new Adapter() });
+
+
 import React from 'react'
-import Button from './index'
 import renderer from 'react-test-renderer'
+import { shallow, mount, render } from 'enzyme';
+import Button from './index'
+
+const noop = () => null
 
 it('Should Button Default to match snapshot', () => {
   const tree = renderer.create(
-    <Button onClick={() => null}>
+    <Button onClick={noop}>
       Click Me
     </Button>
   ).toJSON()
@@ -15,7 +24,7 @@ it('Should Button Default to match snapshot', () => {
 
 it('Should Button Success to match snapshot', () => {
   const tree = renderer.create(
-    <Button onClick={() => null} kind='success'>
+    <Button onClick={noop} kind='success'>
       Click Me Success
     </Button>
   ).toJSON()
@@ -24,9 +33,29 @@ it('Should Button Success to match snapshot', () => {
 
 it('Should Button Danger to match snapshot', () => {
   const tree = renderer.create(
-    <Button onClick={() => null} kind='danger'>
+    <Button onClick={noop} kind='danger'>
       Click Me Danger
     </Button>
   ).toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+it('Should Button with prop kind="success" should has class "-success"', () => {
+  const wrapper = shallow(
+    <Button onClick={noop} kind='success'>
+      Button Success
+    </Button>
+  )
+  expect(wrapper.hasClass('-success')).toBe(true)
+  expect(wrapper.hasClass('-danger')).toBe(false)
+})
+
+it('Should Button with prop kind="danger" should has class "-danger"', () => {
+  const wrapper = shallow(
+    <Button onClick={noop} kind='danger'>
+      Button Danger
+    </Button>
+  )
+  expect(wrapper.hasClass('-danger')).toBe(true)
+  expect(wrapper.hasClass('-success')).toBe(false)
 })
