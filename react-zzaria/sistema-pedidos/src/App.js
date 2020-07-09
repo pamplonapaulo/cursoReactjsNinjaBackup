eb55bd162a99e810fe05bf96584203fe594b5dc1
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-07-08 21:16:08 -0300

    M4 C88

--- react-zzaria/sistema-pedidos/src/App.js
+++ react-zzaria/sistema-pedidos/src/App.js
@@ -1,30 +1,58 @@
-import React, { createContext, lazy, useState, Suspense } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { CssBaseline, LinearProgress } from '@material-ui/core'
+import React, { lazy, Suspense, useContext, useState, useEffect } from 'react'
+import t from 'prop-types'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import { LinearProgress } from '@material-ui/core'
+import firebase from 'services/firebase'
+import { AuthContext } from 'contexts/auth'
 
 const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
-export const ColorContext = createContext()
+function App ({ location }) {
+  const { userInfo, setUserInfo, handleLogout } = useContext(AuthContext)
+  const [didCheckUserIn, setDidCheckUserIn] = useState(false)
 
-function App () {
-  const [color, setColor] = useState('black')
+  const { isUserLoggedIn } = userInfo
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('user data:', user)
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user
+      })
+      setDidCheckUserIn(true)
+    })
+
+    window.handleLogout = handleLogout
+  }, [])
+
+  if (!didCheckUserIn) {
+    return <LinearProgress />
+  }
+
+  if (isUserLoggedIn && location.pathname === '/login') {
+    return <Redirect to='/' />
+  }
+
+  if (!isUserLoggedIn && location.pathname !== '/login') {
+    return <Redirect to='/login' />
+  }
 
   return (
     <>
-      <ColorContext.Provider value={{ color, setColor }}>
-        <CssBaseline />
-        <BrowserRouter>
-          <Suspense fallback={<LinearProgress />}>
-            <Switch>
-              <Route path='/login' component={Login} />
-              <Route path='/' component={MainPage} />
-            </Switch>
-          </Suspense>
-        </BrowserRouter>
-      </ColorContext.Provider>
+      <Suspense fallback={<LinearProgress />}>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/' component={MainPage} />
+        </Switch>
+      </Suspense>
     </>
   )
+}
+
+App.propTypes = {
+  location: t.object.isRequired
 }
 
 export default App
--- react-zzaria/sistema-pedidos/src/pages/main/main.js
+++ react-zzaria/sistema-pedidos/src/pages/main/main.js
@@ -1,25 +1,79 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import styled from 'styled-components'
+import {
+  AppBar,
+  Toolbar as MaterialToolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem
+} from '@material-ui/core'
+import { AccountCircle } from '@material-ui/icons'
+import { ReactComponent as MainLogo } from 'images/logo-react-zzaria.svg'
+import { AuthContext } from 'contexts/auth'
 
-const routes = [
-  { path: '/rota1', content: 'Rota 1' },
-  { path: '/rota2', content: 'Rota 2' }
-]
+const Main = () => {
+  const [anchorElement, setAnchorElement] = useState(null)
+  const { userInfo, handleLogout } = useContext(AuthContext)
 
-const Main = () => (
-  <>
-    <h1>Main</h1>
+  const handleOpenMenu = (e) => {
+    setAnchorElement(e.target)
+  }
 
-    <Switch>
-      {routes.map(route => (
-        <Route
-          key={route.path}
-          path={route.path}
-          render={() => <h2>{route.content}</h2>}
-        />
-      ))}
-    </Switch>
-  </>
-)
+  const handleCloseMenu = () => {
+    setAnchorElement(null)
+  }
+
+  return (
+    <AppBar>
+      <Toolbar>
+        <LogoContainer>
+          <Logo />
+        </LogoContainer>
+
+        <Typography color='inherit'>
+          Hi, {userInfo.user.displayName.split(' ')[0]}
+        </Typography>
+
+        <IconButton color='inherit' onClick={handleOpenMenu}>
+          <AccountCircle />
+        </IconButton>
+
+        <Menu
+          open={Boolean(anchorElement)}
+          onClose={handleCloseMenu}
+          anchorEl={anchorElement}
+        >
+          <MenuItem onClick={handleLogout}>
+            Logout
+          </MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+const Toolbar = styled(MaterialToolbar)`
+  margin: 0 auto;
+  max-width: 960px;
+  width: 100%;
+`
+
+const LogoContainer = styled.div`
+  flex-grow: 1;
+`
+
+const Logo = styled(MainLogo)`
+  height: 50px;
+  width: 200px;
+
+  & path {
+    fill: #fff;
+  }
+
+  & line {
+    stroke: #fff;
+  }
+`
 
 export default Main
