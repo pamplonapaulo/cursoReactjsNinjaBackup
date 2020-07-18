d59bfd6515be77f104d686673fef5a8285629f06
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-07-17 23:44:47 -0300

    M4 C197

---
+++ system-register/src/pages/pizzas-sizes/table.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link, useRouteMatch } from 'react-router-dom'
+import {
+  Button as MaterialButton,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow
+} from '@material-ui/core'
+import { Add, Delete, Edit } from '@material-ui/icons'
+import { TableContainer, TableTitle, Th, THead } from 'ui'
+import { useCollection } from 'hooks'
+import { singularOrPlural } from 'utils'
+import { PIZZAS_SIZES, NEW, EDIT } from 'routes'
+
+function TablePizzasSizes () {
+  const { data: pizzasSizes, remove } = useCollection('pizzasSizes')
+  const newSizePath = useRouteMatch(`${PIZZAS_SIZES}${NEW}`)
+
+  return (
+    <TableContainer>
+      <TitleContainer>
+        <Grid item>
+          <TableTitle>
+            Sizes:
+          </TableTitle>
+        </Grid>
+
+        <Grid item>
+          <Button
+            startIcon={<Add />}
+            color='primary'
+            component={Link}
+            to={`${PIZZAS_SIZES}${NEW}`}
+            disabled={!!newSizePath}
+          >
+            Add new size
+          </Button>
+        </Grid>
+      </TitleContainer>
+
+      <Table>
+        <THead>
+          <TableRow>
+            <Th>Name</Th>
+            <Th>Diameter</Th>
+            <Th>Slices</Th>
+            <Th>Flavours</Th>
+            <Th />
+          </TableRow>
+        </THead>
+
+        <TableBody>
+          {pizzasSizes?.map(pizza => (
+            <TableRow key={pizza.id}>
+              <TableCell>{pizza.name}</TableCell>
+              <TableCell>{pizza.size} cm.</TableCell>
+              <TableCell>{pizza.slices} slices</TableCell>
+              <TableCell>
+                {pizza.flavours} {' '}
+                {singularOrPlural(pizza.flavours, 'flavour', 'flavours')}
+              </TableCell>
+
+              <TableCell align='right'>
+                <Button
+                  startIcon={<Edit />}
+                  component={Link}
+                  to={`${PIZZAS_SIZES}${EDIT(pizza.id)}`}
+                >
+                  Edit
+                </Button>
+
+                <Button
+                  startIcon={<Delete />}
+                  color='secondary'
+                  onClick={() => remove(pizza.id)}
+                >
+                  Remove
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+const TitleContainer = styled(Grid).attrs({
+  container: true,
+  justify: 'space-between',
+  alignItems: 'center'
+})`
+  && {
+    padding: ${({ theme }) => theme.spacing(3)}px;
+
+    ${TableTitle} {
+      padding: 0;
+    }
+  }
+`
+
+const Button = styled(MaterialButton).attrs({
+  variant: 'contained'
+})`
+  && {
+    margin-left: ${({ theme }) => theme.spacing(2)}px
+  }
+`
+
+export default TablePizzasSizes
--- system-register/src/routes.js
+++ system-register/src/routes.js
@@ -2,3 +2,7 @@ export const HOME = '/'
 export const LOGIN = '/login'
 export const PIZZAS_SIZES = '/sizes'
 export const PIZZAS_FLAVOURS = '/flavours'
+
+// actions:
+export const NEW = '/new'
+export const EDIT = (id = ':id') => `/${id}/edit`
