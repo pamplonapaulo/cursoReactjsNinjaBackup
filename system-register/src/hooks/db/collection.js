7a41a965a98471a5c01a626e2c867b4d035c6f39
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-07-19 14:51:26 -0300

    About to deploy the system register

--- system-register/src/hooks/db/collection.js
+++ system-register/src/hooks/db/collection.js
@@ -32,7 +32,7 @@ function useCollection (collection) {
 
   const edit = useCallback((id, data) => {
     console.log('edit size', data, id)
-    return db.collectioncolle(collection).doc(id).set(data)
+    return db.collection(collection).doc(id).set(data)
   }, [collection])
 
   const remove = useCallback(async (id) => {
@@ -40,11 +40,36 @@ function useCollection (collection) {
     fetchCollectionData()
   }, [collection, fetchCollectionData])
 
+  const removePizzaSize = useCallback(async (id) => {
+    const pizzaSizeRef = db.collection('pizzasSizes').doc(id)
+
+    db.runTransaction(async (transaction) => {
+      const sizeDoc = await transaction.get(pizzaSizeRef)
+      if (!sizeDoc.exists) {
+        throw new Error('')
+      }
+      transaction.delete(pizzaSizeRef)
+
+      const allFlavours = await db.collection('pizzasFlavours').get()
+
+      allFlavours.forEach(flavour => {
+        const { [id]: sizeId, ...value } = flavour.data().value
+        const flavourRef = db.collection('pizzasFlavours').doc(flavour.id)
+        transaction.update(flavourRef, { value })
+      })
+    })
+      .then(() => {
+        console.log('Transaction accomplished sucessfully!')
+        fetchCollectionData()
+      })
+      .catch((e) => console.log('Transaction error:', e))
+  }, [fetchCollectionData])
+
   useEffect(() => {
     fetchCollectionData()
   }, [pathname, fetchCollectionData])
 
-  return { data, add, edit, remove }
+  return { data, add, edit, remove, removePizzaSize }
 }
 
 export default useCollection
